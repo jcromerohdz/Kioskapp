9cfa036cca0abe3509dd03014fee14869b120fd1
jcromerohdz <jcromerohdz@gmail.com>
2022-04-18 11:21:28 -0700

    Implementing Kisok context in Next JS

---
+++ context/KioskProvider.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect, createContext } from 'react'
+
+const KioskContext = createContext()
+
+const KioskProvider = ({children}) => {
+
+  return(
+    <KioskContext.Provider
+      value={{
+
+      }}
+    >
+      {children}
+    </KioskContext.Provider>
+  )
+}
+
+export {
+  KioskProvider
+}
+
+export default KioskContext
