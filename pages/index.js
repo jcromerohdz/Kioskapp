ef85146c896f24802d8098dc1e9ad078d700d1a7
jcromerohdz <jcromerohdz@gmail.com>
2022-04-18 09:35:56 -0700

    Get all categories via a Prisma ORM client with Server Side Props in NEXT js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+export default function Home({categories}) {
+  console.log(categories)
   return (
 		<h1>Next.js</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categories = await prisma.categoy.findMany()
+
+  return {
+    props: {
+      categories
+    },
+  }
+
 }
